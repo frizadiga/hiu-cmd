52c388db44f6627b6c7b41fee4703028de116f2e
friza diga <friza diga>
2017-10-07 20:00:22 +0700

    update

--- cmder/bin/quran.cmd
+++ cmder/bin/quran.cmd
@@ -1,1 +1,1 @@
-chrome --profile-directory="Default" --app="data:text/html,<html><body><script>function loadURL(cb){resizeTo(550,750);location='https://quran.com/%1/%2';cb();}loadURL(function(){document.title='Google Translate Powered by hiuWave';});</script></body></html>"
+chrome --profile-directory="Default" --app="data:text/html,<html><body><script>resizeTo(550,750);location='https://quran.com/%1/%2';;</script></body></html>"
