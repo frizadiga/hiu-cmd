698fe2da8d1dd2da35b28c1fc7f14621a427e98d
friza diga <friza diga>
2017-10-23 16:13:16 +0700

    update

--- cmder/bin/google-translate.cmd
+++ cmder/bin/google-translate.cmd
@@ -1,1 +1,1 @@
-chrome --profile-directory="Default" --app="data:text/html,<html><body><script>resizeTo(797,297);location='https://translate.google.com';</script></body></html>"
+chrome --profile-directory="Default" --app="data:text/html,<html><body><script>resizeTo(810,310);location='https://translate.google.com';</script></body></html>"
