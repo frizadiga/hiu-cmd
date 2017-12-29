43df79d8d361de21c9baefac7c03ff30e885b603
frizadiga <frizadiga@gmail.com>
2017-12-29 15:15:41 +0700

    enhancing her command feature

--- cmder/bin/her.cmd
+++ cmder/bin/her.cmd
@@ -1,1 +1,8 @@
-@echo hiu-env root path & cd /d "%CMDER_ROOT%" & cd../..
+@echo off
+
+if [%1]==[] echo (root path) hiu-env
+
+cd /d "%CMDER_ROOT%" & cd../..
+
+if "%1" == "cmd" echo (root path) hiu-cmd & cd hiu-cmd
+if "%1" == "text" echo (root path) hiu-text & cd hiu-text
