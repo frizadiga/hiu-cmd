a84805632b68c312b6010253b157cb204422554d
frizadiga <frizadiga@gmail.com>
2018-02-13 14:20:57 +0700

    improve pma to able passing dynamic DB

--- cmder/bin/pma.cmd
+++ cmder/bin/pma.cmd
@@ -1,1 +1,11 @@
-@echo phpmyadmin Apps by hiuWave & appchr http://localhost:8888/phpmyadmin 1100 800
+@echo phpmyadmin Apps by hiuWave
+
+rem @echo off
+
+if "%1"=="" echo (root path) hiu-env
+
+cd /d "%CMDER_ROOT%" & cd../..
+
+if "%1" == "local" echo (root path) pma localhost & appchr http://localhost:8888/phpmyadmin 1100 800
+
+if "%1" == "circledoo-dev" echo (root path) pma circledoo-dev & appchr "http://159.65.13.76:8000/" 1100 800
