9d507f82386d886c99def30dd65657b89053a0e8
frizadiga <frizadiga@gmail.com>
2018-04-24 14:03:38 +0700

    add new command

--- cmder/bin/pma.cmd
+++ cmder/bin/pma.cmd
@@ -9,3 +9,5 @@ cd /d "%CMDER_ROOT%" & cd../..
 if "%1" == "local" echo (root path) pma localhost & appchr http://localhost:8888/phpmyadmin 1100 800
 
 if "%1" == "circledoo-dev" echo (root path) pma circledoo-dev & appchr "http://159.65.13.76:8000/" 1100 800
+
+if "%1" == "circledoo-prod" echo (root path) pma circledoo-dev & appchr "http://db.circledoo.com/phpmyadmin/" 1100 800
